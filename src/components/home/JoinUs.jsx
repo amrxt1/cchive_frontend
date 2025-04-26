9e51f7ad6b93b0aa59741db637c6ed7e883b7034
amrxt1 <amrxt11@outlook.com>
2025-04-25 19:56:22 -0700

    Restyle home/JoinUs.jsx and make it match the aesthetic

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -4,17 +4,23 @@ import Container from "../shared/Container";
 const JoinUs = () => {
   return (
     <Container>
-      <div className="mx-4 my-8 p-3 font-special flex flex-col gap-3 rounded-2xl bg-text text-background lg:gap-8 lg:py-32 lg:px-64 lg:mx-32">
-        <h1 className="text-3xl ">Join the Fun</h1>
-        <div className=" text-2xl">
-          Since you've scrolled thus far, we would recommend joining and
-          exploring what you've just read about. Networking, Marketplace,
-          Collaboration, and much more... all at CC Hive! and the fun part?{" "}
-          <br /> It's exclusive to only the students!
+      <div
+        className="p-4 font-special rounded-2xl bg-background text-text 
+                      border-2 border-accent
+                      lg:gap-8 lg:py-32 lg:px-64 lg:mx-32"
+      >
+        <div className="pb-4">
+          <h1 className="text-3xl ">Join the Fun</h1>
+          <div className=" text-2xl">
+            Since you've scrolled thus far, we would recommend joining and
+            exploring what you've just read about. Networking, Marketplace,
+            Collaboration, and much more... all at CC Hive! and the fun part?{" "}
+            <br /> It's exclusive to only the students!
+          </div>
         </div>
         <div className="flex">
           <Link to={"/register"} className="flex-1 ">
-            <button className=" bg-accent px-3 py-1 rounded hover:bg-background hover:text-accent w-[100%]">
+            <button className=" bg-accent px-3 py-1 rounded border-2 hover:bg-background hover:text-accent w-full ">
               Get Started
             </button>
           </Link>
