c03bd725ca6fcaa8936b2455f59a30dbbff3aa1e
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-07-25 18:31:27 -0700

    Improve styling for JoinUs.jsx

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -5,21 +5,19 @@ const JoinUs = () => {
     <Container className="pt-16">
       <div
         className="p-16 text-center font-special rounded-2xl
-                      border-2 border-primary
-                      lg:gap-8 lg:py-16 lg:px-64 bg-surface shadow-md hover:shadow-lg"
+                      border-2 border-primary flex justify-center
+                      bg-surface shadow-md hover:shadow-lg"
       >
-        <div className="pb-8">
-          <h1 className="text-4xl font-bold text-primary mb-4">Join the Fun</h1>
-          <div className=" text-2xl text-left">
+        <div className="pb-8 w-[50%] flex flex-col gap-12">
+          <h1 className="text-4xl font-bold text-primary">Join the Fun</h1>
+          <div className=" text-2xl text-left font-display">
             Since you've scrolled thus far, we would recommend joining and
             exploring. <br /> Networking, Marketplace, Collaboration, and much
             more... all at{" "}
             <span className="text-primary font-semibold">CC Hive</span>
             ! and the fun part? <br /> It's exclusive to only the students!
           </div>
-        </div>
-        <div className="flex">
-          <Link to={"/register"} className="flex-1">
+          <Link to={"/register"}>
             <button
               className=" bg-accent px-3 py-1 rounded border-2 border-accent text-background
                             hover:bg-background hover:text-accent w-full
