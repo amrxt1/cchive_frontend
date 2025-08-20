2a7fc4888bd1fdb03376075471a1f4e9fb98f8f7
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-08-19 19:28:19 -0700

    Style Courses.jsx along with a Search bar

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,23 +1,38 @@
 import { useCourses } from "../hooks/useCourses";
 import { Link } from "react-router-dom";
 import Container from "../components/shared/Container";
+import { useState } from "react";
 
 const Courses = () => {
   const { data: courses, isLoading, error } = useCourses();
+  const [search, setSearch] = useState("");
+
+  const searchResult =
+    courses?.filter((course) =>
+      course.title.toLowerCase().includes(search.toLowerCase()),
+    ) || [];
 
   return (
     <Container>
       <div>
-        <h1>Courses 📚</h1>
+        <h1 className="text-primary mt-4 text-2xl font-bold">Courses 📚</h1>
 
         {isLoading && <p>Loading courses...</p>}
         {error && <p className="text-red-500">Failed to load courses.</p>}
 
-        <div className="grid grid-cols-3 gap-4">
-          {courses?.map((course) => (
-            <div className="px-4 py-8 bg-surface rounded-lg">
-              <Link to={`/courses/${course.id}`} key={course.id}>
-                <h3 className="text-primary font-semibold text-xl hover:underline">
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-background text-md border-surface mt-2 w-full rounded-md border-1 px-2 py-1"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="mt-4 grid grid-cols-1 gap-4">
+          {searchResult?.map((course) => (
+            <div className="bg-surface rounded-lg px-4 py-8" key={course.id}>
+              <Link to={`/courses/${course.id}`}>
+                <h3 className="text-primary text-xl font-semibold hover:underline">
                   {course.title}
                 </h3>
                 <p>
