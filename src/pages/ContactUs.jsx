37b81c503de2f3bc5aa56cc8961a5740e92c1f52
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-08-22 18:35:42 -0700

    Add a ContactUs form UI only

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import Container from "../components/shared/Container";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <Container>
+      <h1 className="text-primary mt-4 text-center text-2xl font-bold">
+        Contact Us
+      </h1>
+      <form className="bg-surface mt-4 grid gap-y-4 rounded-md px-2 py-4">
+        <input
+          type="text"
+          className="bg-background text-md rounded-md px-2 py-1"
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          className="bg-background text-md rounded-md px-2 py-1"
+          placeholder="Last Name"
+        />
+        <input
+          type="text"
+          className="bg-background text-md rounded-md px-2 py-1"
+          placeholder="Your Email"
+        />
+        <input
+          type="text"
+          className="bg-background text-md rounded-md px-2 py-1"
+          placeholder="Subject"
+        />
+        <textarea
+          type="textarea"
+          className="bg-background text-md resize-none rounded-md px-2 py-1"
+          placeholder="Message"
+          rows={4}
+        />
+        <button className="bg-primary text-surface w-full rounded-lg p-1 text-lg font-bold">
+          Submit
+        </button>
+      </form>
+    </Container>
+  );
 };
 
 export default ContactUs;
