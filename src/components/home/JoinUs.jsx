18f76bd22b08b797a02b04ce5f871110d6927c4e
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-08-22 18:36:16 -0700

    Improve JoinUs.jsx aesthetic to match the overall style

--- src/components/home/JoinUs.jsx
+++ src/components/home/JoinUs.jsx
@@ -3,11 +3,11 @@ import Container from "../shared/Container";
 const JoinUs = () => {
   return (
     <Container className="relative my-32" noPadding>
-      <div className="absolute inset-0 -z-10 bg-gradient-to-tr from-pink-100 via-yellow-100 to-purple-100" />
-      <div className="pointer-events-none absolute -top-10 -left-10 -z-10 h-96 w-96 animate-pulse rounded-full bg-pink-300 opacity-30 mix-blend-multiply blur-3xl filter" />
-      <div className="pointer-events-none absolute right-10 bottom-0 -z-10 h-96 w-96 animate-bounce rounded-full bg-purple-300 opacity-25 mix-blend-multiply blur-3xl filter" />
-      <div className="font-special border-primary flex justify-center bg-none p-4 py-16 text-center shadow-md hover:shadow-lg md:p-16">
-        <div className="bg-surface/81 flex w-full flex-col gap-12 rounded-lg p-8 pb-8 shadow-lg backdrop-blur-2xl md:w-[50%] lg:w-[40%] xl:w-[33%]">
+      <div class="bg-primary/17 absolute -top-10 -left-10 h-96 w-96 rounded-full blur-3xl"></div>
+      <div class="bg-accent/17 absolute right-10 bottom-0 h-96 w-56 rounded-full blur-3xl"></div>
+
+      <div className="font-special flex justify-center bg-none p-4 py-16 text-center md:p-16">
+        <div className="bg-surface/34 flex w-full flex-col gap-12 rounded-lg p-8 pb-8 shadow-lg backdrop-blur-2xl md:w-[50%] lg:w-[40%] xl:w-[33%]">
           <h1 className="text-primary text-4xl font-bold">Join the Fun</h1>
           <p className="font-display lg:text-2xl">
             Since you've scrolled thus far, we would recommend joining and
